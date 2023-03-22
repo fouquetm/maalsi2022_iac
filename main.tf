9ac4cda44d74bb43d819e02bf81ce397b95c35c5
Matthieu FOUQUET <mfouquet@sqli.com>
2023-03-22 12:07:15 +0100

    adding data sources

--- main.tf
+++ main.tf
@@ -11,23 +11,18 @@ provider "azurerm" {
   features {}
 }
 
-resource "azurerm_resource_group" "rg-maalsi" {
-  name     = "rg-${var.project_name}${var.environment_suffix}"
-  location = var.location
-}
-
 resource "azurerm_service_plan" "app-plan" {
   name                = "plan-${var.project_name}${var.environment_suffix}"
-  resource_group_name = azurerm_resource_group.rg-maalsi.name
-  location            = azurerm_resource_group.rg-maalsi.location
+  resource_group_name = data.azurerm_resource_group.rg-maalsi.name
+  location            = data.azurerm_resource_group.rg-maalsi.location
   os_type             = "Linux"
   sku_name            = "P1v2"
 }
 
 resource "azurerm_linux_web_app" "webapp" {
   name                = "web-${var.project_name}${var.environment_suffix}"
-  resource_group_name = azurerm_resource_group.rg-maalsi.name
-  location            = azurerm_resource_group.rg-maalsi.location
+  resource_group_name = data.azurerm_resource_group.rg-maalsi.name
+  location            = data.azurerm_resource_group.rg-maalsi.location
   service_plan_id     = azurerm_service_plan.app-plan.id
 
   site_config {}

10648e7cc6a7297448f4a146bbf73d33e77e1cdb
Matthieu FOUQUET <mfouquet@sqli.com>
2023-03-22 11:11:25 +0100

    ajout de blocs output

--- main.tf
+++ main.tf
@@ -15,3 +15,11 @@ resource "azurerm_resource_group" "rg-maalsi" {
   name     = "rg-mfouquet"
   location = "West Europe"
 }
+
+output "main-rg-name" {
+  value = azurerm_resource_group.rg-maalsi.name
+}
+
+output "main-rg-id" {
+  value = azurerm_resource_group.rg-maalsi.id
+}

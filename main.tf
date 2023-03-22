5fefc480daae37a48023873c51c58c5992671909
Matthieu FOUQUET <mfouquet@sqli.com>
2023-03-22 15:09:28 +0100

    adding provisioning condition for dev env

--- main.tf
+++ main.tf
@@ -24,6 +24,7 @@ resource "azurerm_service_plan" "app-plan" {
 }
 
 resource "azurerm_linux_web_app" "webapp" {
+  count               = var.environment_suffix == "-dev" ? 1 : 0    # n'est créé qu'en env de dev
   name                = "web-${var.project_name}${var.environment_suffix}"
   resource_group_name = data.azurerm_resource_group.rg-maalsi.name
   location            = data.azurerm_resource_group.rg-maalsi.location

1e16917c4a531387a0cae959474cdbb481a23773
Matthieu FOUQUET <mfouquet@sqli.com>
2023-03-23 09:41:29 +0100

    adding webapp and connectionstring configuration

--- main.tf
+++ main.tf
@@ -34,3 +34,25 @@ resource "azurerm_mssql_database" "sql-db" {
   sku_name       = "S0"
   zone_redundant = false
 }
+
+resource "azurerm_service_plan" "app-plan" {
+  name                = "plan-${var.project_name}${var.environment_suffix}"
+  resource_group_name = data.azurerm_resource_group.rg-maalsi.name
+  location            = data.azurerm_resource_group.rg-maalsi.location
+  os_type             = "Linux"
+  sku_name            = "S1"
+}
+
+resource "azurerm_linux_web_app" "webapp" {
+  name                = "web-${var.project_name}${var.environment_suffix}"
+  resource_group_name = data.azurerm_resource_group.rg-maalsi.name
+  location            = data.azurerm_resource_group.rg-maalsi.location
+  service_plan_id     = azurerm_service_plan.app-plan.id
+
+  site_config {}
+
+  connection_string {
+    name = "DefaultConnection"
+    value = "Server=tcp:${azurerm_mssql_server.sql-srv.fully_qualified_domain_name},1433;Initial Catalog=${azurerm_mssql_database.sql-db.name};Persist Security Info=False;User ID=${data.azurerm_key_vault_secret.database-login.value};Password=${azurerm_key_vault_secret.database-password.value};MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;"
+  }
+}

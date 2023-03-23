19255386210ea35fbff36033e812064fb0097aaf
Matthieu FOUQUET <mfouquet@sqli.com>
2023-03-23 10:28:37 +0100

    adding rabbitmq container group

--- main.tf
+++ main.tf
@@ -14,7 +14,9 @@ terraform {
 provider "azurerm" {
   features {}
 }
-
+###############
+# Database
+###############
 resource "azurerm_mssql_server" "sql-srv" {
   name                         = "sqlsrv-${var.project_name}${var.environment_suffix}"
   resource_group_name          = data.azurerm_resource_group.rg-maalsi.name
@@ -35,6 +37,9 @@ resource "azurerm_mssql_database" "sql-db" {
   zone_redundant = false
 }
 
+###############
+# API Web App
+###############
 resource "azurerm_service_plan" "app-plan" {
   name                = "plan-${var.project_name}${var.environment_suffix}"
   resource_group_name = data.azurerm_resource_group.rg-maalsi.name
@@ -58,8 +63,42 @@ resource "azurerm_linux_web_app" "webapp" {
   }
 
   app_settings = {
-    "RabbitMQ__Hostname" = "rabbitmq",
+    "RabbitMQ__Hostname" = azurerm_container_group.rabbitmq.fqdn,
     "RabbitMQ__Username" = data.azurerm_key_vault_secret.rabbitmq-login.value,
     "RabbitMQ__Password" = data.azurerm_key_vault_secret.rabbitmq-password.value
+  }
+}
+
+###############
+# RabbitMQ : Container Instance
+###############
+resource "azurerm_container_group" "rabbitmq" {
+  name                = "aci-mq-${var.project_name}${var.environment_suffix}"
+  resource_group_name = data.azurerm_resource_group.rg-maalsi.name
+  location            = data.azurerm_resource_group.rg-maalsi.location
+  ip_address_type     = "Public"
+  dns_name_label      = "aci-mq-${var.project_name}${var.environment_suffix}"
+  os_type             = "Linux"
+
+  container {
+    name   = "rabbitmq"
+    image  = "rabbitmq:3-management"
+    cpu    = "0.5"
+    memory = "1.5"
+
+    ports {
+      port     = 5672
+      protocol = "TCP"
+    }
+
+    ports {
+      port     = 15672
+      protocol = "TCP"
+    }
+
+    environment_variables = {
+      "RABBITMQ_DEFAULT_USER" = data.azurerm_key_vault_secret.rabbitmq-login.value,
+      "RABBITMQ_DEFAULT_PASS" = data.azurerm_key_vault_secret.rabbitmq-password.value
+    }
   }
 }
